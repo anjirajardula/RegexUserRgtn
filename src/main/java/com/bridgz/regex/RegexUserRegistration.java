fc0f177cdfb18869eecaf3eb0df1151e392d8f15
anjirajardula <anjirajardula@gmail.com>
2021-09-15 22:35:34 +0530

    Added: user mobile number

--- src/main/java/com/bridgz/regex/RegexUserRegistration.java
+++ src/main/java/com/bridgz/regex/RegexUserRegistration.java
@@ -6,13 +6,23 @@ import java.util.regex.Pattern;
 
 public class RegexUserRegistration extends UserDetails {
     static Scanner scanner = new Scanner(System.in);
-    static String firstName, lastName, emailId;
+    static String firstName, lastName, emailId, mobileNum;
 
     public static void main(String[] args) {
         userFirstName();
         userLastName();
         userEmailId();
-        System.out.println(firstName + " " + lastName + '\n' + emailId);
+        userMobileNum();
+        System.out.println(firstName + " " + lastName + '\n' + emailId + '\n' + mobileNum);
+    }
+
+
+    private static void userMobileNum() {
+
+        System.out.println("Enter your MobileNumber");
+        mobileNum = scanner.next();
+        UserDetails.mobilenum(mobileNum);
+
     }
 
     public static void userFirstName() {
@@ -32,4 +42,6 @@ public class RegexUserRegistration extends UserDetails {
         emailId = scanner.next();
         UserDetails.emailid(emailId);
     }
+
 }
+
