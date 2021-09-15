e992f1992bcdf047ad8c7f9607e6884c041225c3
anjirajardula <anjirajardula@gmail.com>
2021-09-15 21:49:49 +0530

    Added:user email id

--- src/main/java/com/bridgz/regex/RegexUserRegistration.java
+++ src/main/java/com/bridgz/regex/RegexUserRegistration.java
@@ -6,22 +6,30 @@ import java.util.regex.Pattern;
 
 public class RegexUserRegistration extends UserDetails {
     static Scanner scanner = new Scanner(System.in);
-    static String firstName,lastName;
+    static String firstName, lastName, emailId;
 
     public static void main(String[] args) {
         userFirstName();
         userLastName();
-        System.out.println(firstName + " " + lastName);
+        userEmailId();
+        System.out.println(firstName + " " + lastName + '\n' + emailId);
     }
+
     public static void userFirstName() {
         System.out.println("Enter your First Name");
         firstName = scanner.next();
         UserDetails.firstname(firstName);
     }
+
     public static void userLastName() {
         System.out.println("Enter your Last Name");
         lastName = scanner.next();
         UserDetails.lastname(lastName);
     }
 
+    public static void userEmailId() {
+        System.out.println("Enter your EmailId");
+        emailId = scanner.next();
+        UserDetails.emailid(emailId);
+    }
 }
